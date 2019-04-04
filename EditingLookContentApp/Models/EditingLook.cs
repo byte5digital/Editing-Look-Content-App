4f541a36a14783ae7e5760627bd6758459a68a3d
Paraman <helmut_wayne-trapp@t-online.de>
2019-04-04 15:52:45 +0200

    Implemented EditingLook Model

---
+++ EditingLookContentApp/Models/EditingLook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace byte5.EditingLookContentApp.Models
+{
+    public class EditingLook
+    {
+        public int Id { get; set; }
+
+        public string NodeUid { get; set; }
+
+        public int UserId { get; set; }
+        
+        public DateTime SubscribeDate { get; set; }
+
+
+        public EditingLook() { }
+
+        public EditingLook(int id, string nodeUid, int userId, DateTime subscribeDate)
+        {
+            this.Id = id;
+            this.NodeUid = nodeUid;
+            this.UserId = UserId;
+            this.SubscribeDate = subscribeDate;
+        }
+    }
+}

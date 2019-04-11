0217ffc4d4de0bed9212bd5ef5388e70dcaa47bb
Sören Deger <sdeger@byte5.de>
2019-04-11 10:18:53 +0200

    Aktueller Entwicklungsstand von Lukas

--- EditingLookContentApp/App_Plugins/byte5EditingLookContentApp/editingLook.controller.js
+++ EditingLookContentApp/App_Plugins/byte5EditingLookContentApp/editingLook.controller.js
@@ -1,12 +1,92 @@
 ﻿angular.module("umbraco")
-    .controller("byte5.EditingLookContentApp", function ($scope) {
+    .controller("byte5.EditingLookContentApp", function ($scope, byte5EditingLookResource) {
+        var vm = this;
+        vm.currentNodeId = editorState.current.udi;
 
-        // Change this to fit the needs of the container.
-        // It shows how to set a class on a certain node.
-        // We need to set is-locked to show the lock icon. See editingLook.css.
-        $scope.setLocked = function () {
-            $scope.currentNode.cssClasses.push("is-locked");
-            // to remove the lock just remove "is-locked" from cssClasses
-        };
-       
+        byte5EditingLookResource.getCurrentUser(vm.CurrentNodeId).then(function (response) {
+
+            if (response.Status === 200) {
+                if (!response.data) {
+                    //No user found
+                    lockNode(vm, byte5EditingLookResource);
+                }
+                else if (response.data.UserId === vm.currentUserId) {
+                    //Current user locks the node, so the node isnt locked for him
+                    setNodeStatus(vm, 'Success');
+                }
+                else {
+                    //User found isnt the current user, so the node needs to be locked
+                    setNodeStatus(vm, 'Alert');
+                }
+            } else {
+                //If the API returns an error the node wont get locked, so we need to set the status to success
+                setNodeStatus(vm, 'Success');
+            }
+        });
     });
+
+function setNodeStatus(vm, status) {
+    switch (status) {
+        case 'Success':
+            vm.lockNode = false;
+            break;
+        case 'Alert':
+            vm.lockNode = true;
+            break;
+    }
+    vm.badgeStatus = status;
+}
+
+function lockNode(vm, resource) {
+    resource.createEditingEntry(vm.currentNodeId, vm.currentUserId).then(function (response) {
+        if (response.Status === 200) {
+            setNodeStatus(vm, 'Success');
+            keepLockAlive($scope);
+            lockView();
+        } else {
+            //Error node could not be locked
+        }
+    });
+}
+
+function unlockNode(vm, resource) {
+    resource.deleteEditingEntry(vm.currentNodeId, vm.currentUserId).then(function (response) {
+        if (response.Status === 200) {
+            //User left page
+        } else {
+            //Error node could not be unlocked - node will automatically be unlocked after 2 mins
+        }
+    });
+}
+function lockView() {
+    //Html Style changes etc...
+}
+
+function unlockView() {
+    //Html Style changes etc...
+}
+
+function keepLockAlive(scope) {
+    $interval(function () {
+        t++;
+
+        console.log(t);
+
+    }, 1000, 5);
+}
+
+function registerEvents(scope) {
+    scope.$on(
+        "$locationChangeSuccess",
+        handleLocationChangeEvent(event)
+    );
+}
+
+function handleLocationChangeEvent(event) {
+
+    console.log("Location Change from " + vm.currentPath + " to  " + $location.path());
+
+    alert("Finished!");
+
+}
+
--- EditingLookContentApp/App_Plugins/byte5EditingLookContentApp/editingLook.resource.js
+++ EditingLookContentApp/App_Plugins/byte5EditingLookContentApp/editingLook.resource.js
@@ -1,8 +1,30 @@
 ﻿angular.module('umbraco.resources').factory('byte5EditingLookResource',
     function ($q, $http, umbRequestHelper) {
         return {
-            
+            getCurrentUser: function (currentUdi) {
+                return umbRequestHelper.resourcePromise(
+                    $http.get("backoffice/b5LinkedNodes/LinkedNodesContentAppApi/GetLinkedNodes?currentUdi="
+                        + currentUdi),
+                    "Failed to retrieve linked nodes for this node");
+            },
+            createEditingEntry: function (currentUdi, currentUserId) {
+                return umbRequestHelper.resourcePromise(
+                    $http.get("backoffice/b5LinkedNodes/LinkedNodesContentAppApi/GetLinkedNodes?currentUdi="
+                        + currentUdi + "&isContent=" + currentUserId),
+                    "Failed to retrieve linked nodes for this node");
+            },
+            UpdateEditingEntry: function (currentUdi, currentUserId) {
+                return umbRequestHelper.resourcePromise(
+                    $http.get("backoffice/b5LinkedNodes/LinkedNodesContentAppApi/GetLinkedNodes?currentUdi="
+                        + currentUdi + "&isContent=" + currentUserId),
+                    "Failed to retrieve linked nodes for this node");
+            },
+            deleteEditingEntry: function (currentUdi, currentUserId) {
+                return umbRequestHelper.resourcePromise(
+                    $http.get("backoffice/b5LinkedNodes/LinkedNodesContentAppApi/GetLinkedNodes?currentUdi="
+                        + currentUdi + "&isContent=" + currentUserId),
+                    "Failed to retrieve linked nodes for this node");
+            }
         };
     }
 );
-

5ad79df8c1223ac6c7aaa6aa938d73dcd75251ca
Jonathan Distenfeld <jonathan.distenfeld@modulacht.de>
2019-05-07 10:02:09 +0200

    edit api calls

--- EditingLookContentApp/App_Plugins/byte5EditingLookContentApp/editingLook.resource.js
+++ EditingLookContentApp/App_Plugins/byte5EditingLookContentApp/editingLook.resource.js
@@ -3,27 +3,27 @@
         return {
             getCurrentUser: function (currentUdi) {
                 return umbRequestHelper.resourcePromise(
-                    $http.get("backoffice/b5LinkedNodes/LinkedNodesContentAppApi/GetLinkedNodes?currentUdi="
+                    $http.get("backoffice/byte5EditingLook/api/getcurrentuser?nodeUid="
                         + currentUdi),
-                    "Failed to retrieve linked nodes for this node");
+                    "Failed to retrieve the current user for this node");
             },
             createEditingEntry: function (currentUdi, currentUserId) {
                 return umbRequestHelper.resourcePromise(
-                    $http.get("backoffice/b5LinkedNodes/LinkedNodesContentAppApi/GetLinkedNodes?currentUdi="
-                        + currentUdi + "&isContent=" + currentUserId),
-                    "Failed to retrieve linked nodes for this node");
+                    $http.get("backoffice/byte5EditingLook/api/createeditingentry?nodeUid="
+                        + currentUdi + "&currentUserId=" + currentUserId),
+                    "Failed to create entry for this node");
             },
             UpdateEditingEntry: function (currentUdi, currentUserId) {
                 return umbRequestHelper.resourcePromise(
-                    $http.get("backoffice/b5LinkedNodes/LinkedNodesContentAppApi/GetLinkedNodes?currentUdi="
-                        + currentUdi + "&isContent=" + currentUserId),
-                    "Failed to retrieve linked nodes for this node");
+                    $http.get("backoffice/byte5EditingLook/api/updateeditingentry?nodeUid="
+                        + currentUdi + "&currentUserId=" + currentUserId),
+                    "Failed to update entry for this node");
             },
             deleteEditingEntry: function (currentUdi, currentUserId) {
                 return umbRequestHelper.resourcePromise(
-                    $http.get("backoffice/b5LinkedNodes/LinkedNodesContentAppApi/GetLinkedNodes?currentUdi="
-                        + currentUdi + "&isContent=" + currentUserId),
-                    "Failed to retrieve linked nodes for this node");
+                    $http.get("backoffice/byte5EditingLook/api/deleteeditingentry?nodeUid="
+                        + currentUdi + "&currentUserId=" + currentUserId),
+                    "Failed to delete entry for this node");
             }
         };
     }

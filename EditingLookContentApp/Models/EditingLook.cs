a67a37b066f911d29d8edf6934c9b55916277c38
Jonathan Distenfeld <jonathan.distenfeld@modulacht.de>
2019-05-07 09:52:40 +0200

    Autoincrease id column (db was recreated by temp code)

--- EditingLookContentApp/Models/EditingLook.cs
+++ EditingLookContentApp/Models/EditingLook.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿using NPoco;
+using System;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
 
 namespace byte5.EditingLookContentApp.Models
 {
+    [TableName("EditingLook")]
+    [PrimaryKey("Id", AutoIncrement = true)]
+    [ExplicitColumns]
     public class EditingLook
     {
+        [Column("Id")]
+        [PrimaryKeyColumn(AutoIncrement = true)]
         public int Id { get; set; }
 
+        [Column("NodeUid")]
         public string NodeUid { get; set; }
 
+        [Column("UserId")]
         public int UserId { get; set; }
-        
+
+        [Column("SubscribeDate")]
         public DateTime SubscribeDate { get; set; }
     }
 }
